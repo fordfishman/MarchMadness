deba1cb721875dfe12cd1117046d9d496d7d3c8b
Ford Fishman <fordfishman@gmail.com>
2022-03-16 15:48:01 -0400

    python comment formatting, removing deprecations

--- march_madness.py
+++ march_madness.py
@@ -1,56 +1,54 @@
 #!/usr/bin/env python
+## Ford Fishman
 
-##This program uses Five Thirty Eight's NCAAM power rankings (made through combining BPI, ELO, and other sources)
-##to predict an outcome of March Madness.  If teams win, their ranking is slightly increased.  If an underdog 
-##wins, then their ranking is improved based upon how much worse they were considered than the other team
-##by a tier-based approach.  
+## This program uses Five Thirty Eight's NCAAM power rankings (made through combining BPI, ELO, and other sources)
+## to predict an outcome of March Madness.  If teams win, their ranking is slightly increased.  If an underdog 
+## wins, then their ranking is improved based upon how much worse they were considered than the other team
+## by a tier-based approach.  
 
-##Set up argparse to take in command line arguments
+## Set up argparse to take in command line arguments
 
-#teams: names of teams in order of power rankings; FORMAT: One team name per line (i.e. Miami(FL)\n)
-#rankings: power rankings greatest to least; FORMAT: One rank per line (98.2\n)
-#DEPRECATED!! - randomnumbers: randomly generated integers 1-100 (currently using R, would like to do it natively in Python); FORMAT: All integers spaced on one line separated by spaces(4 23 41 51...)
-#firstround: first round matchups; FORMAT: matchups arranged by region (east, west, midwest, south), team names separated by space in single line, one matchup per row (Wisconsin VirginiaTech\n)
+# teams: names of teams in order of power rankings; FORMAT: One team name per line (i.e. Miami(FL)\n)
+# rankings: power rankings greatest to least; FORMAT: One rank per line (98.2\n)
+# firstround: first round matchups; FORMAT: matchups arranged by region (east, west, midwest, south), team names separated by space in single line, one matchup per row (Wisconsin VirginiaTech\n)
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-t','--teams')
 parser.add_argument('-r','--rankings')
-#parser.add_argument('--randomnumbers')
 parser.add_argument('-f','--firstround')
 parser.add_argument('-o','--output', default = 'MarchMadnessResults.txt')
 arguments = parser.parse_args()
 teams_filename = arguments.teams
 rankings_filename = arguments.rankings
-#randomnumbers_filename = arguments.randomnumbers
 firstround_filename = arguments.firstround
 output = arguments.output
 
-#List to contain team names in order of original power ranking
+# List to contain team names in order of original power ranking
 teams_list = []
-#Dict. to contain power rankings in original order
+# Dict to contain power rankings in original order
 team_rank = {}
 
-#Imports team names
+# Imports team names
 with open(teams_filename, 'r') as teamfile:
 	for team in teamfile:
 		team = team.strip()
 		teams_list.append(team)
 
-#Imports power rankings, and creates a dictionary entry for each team of form TeamName: Powerrank
+# Imports power rankings, and creates a dictionary entry for each team of form TeamName: Powerrank
 with open(rankings_filename, 'r') as rankingsfile:
 	for index, rank in enumerate(rankingsfile):
 		rank = float(rank.strip())
 		team_rank[teams_list[index]] = rank
 
 
-#Creates a set of random numbers to determine wins
+# Creates a set of random numbers to determine wins
 from random import *
 ran_num = []
 for x in range(63):
 	ran_num.append(uniform(1,100))
 
-#Imports first round matchups
+# Imports first round matchups
 matchups= []
 with open(firstround_filename, 'r') as firstround_file:
 	for index, line in enumerate(firstround_file):
@@ -59,19 +57,19 @@ with open(firstround_filename, 'r') as firstround_file:
 		game[1] = game[1].strip()
 		matchups.append(game)
 
-#Defines regions
+# Defines regions
 east = matchups[0:8]
 west = matchups[8:16]
 midwest = matchups[16:24]
 south = matchups[24:32]
 
 
-#Defining main function "probability."  Takes the team power ranks of the matchup and converts it into form:
-#r = 10^(PR/10)
-#Where PR is the power rank, and r is new rank.  
-#New ranks are compared by form of 1. (rank2/rank1+rank2) or 2. (rank1/rank1+rank2) (depending on which rank is higher
-#This determines probability of victory.  If (random number)/100 in case 1. is above probability, then team 1 wins.
-#If a team wins, its rating is increased.  If the winning team's power ranking was lower, it will raised according to how much lower it was.
+# Defining main function "probability."  Takes the team power ranks of the matchup and converts it into form:
+# r = 10^(PR/10)
+# Where PR is the power rank, and r is new rank.  
+# New ranks are compared by form of 1. (rank2/rank1+rank2) or 2. (rank1/rank1+rank2) (depending on which rank is higher
+# This determines probability of victory.  If (random number)/100 in case 1. is above probability, then team 1 wins.
+# If a team wins, its rating is increased.  If the winning team's power ranking was lower, it will raised according to how much lower it was.
 def probability(team1, team2, ind):
 	rank1 = 10 ** (team_rank[team1]/10)
 	rank2 = 10 ** (team_rank[team2]/10)
@@ -119,12 +117,12 @@ def probability(team1, team2, ind):
 				team_rank[team2] += 4.0
 	return winner
 
-#The rest of the code will go region by region until the final four and championship games
+# The rest of the code will go region by region until the final four and championship games
 
-#establishes index for random number cycling
+# establishes index for random number cycling
 i = 0
 
-#ROUNDOF64
+# ROUNDOF64
 eastfirst = []
 for game in east:
 	win = probability(game[0], game[1], i)
@@ -206,7 +204,7 @@ for game in south:
 	if win in south[7]:
 		southfirst[3].append(win)
 
-#ROUNDOF32
+# ROUNDOF32
 eastsecond = []
 for game in eastfirst:
 	win = probability(game[0], game[1], i)
@@ -256,7 +254,7 @@ for game in southfirst:
 	if win in southfirst[3]:
 		southsecond[1].append(win)
 
-#SWEETSIXTEEN
+# SWEETSIXTEEN
 eastthird = []
 for game in eastsecond:
 	win = probability(game[0], game[1], i)
@@ -290,7 +288,7 @@ for game in southsecond:
 	if win in southsecond[1]:
 		souththird[0].append(win)
 
-#ELITEEIGHT
+# ELITEEIGHT
 for game in eastthird:
 	win = probability(game[0], game[1], i)
 	i += 1
@@ -308,16 +306,16 @@ for game in souththird:
 	i += 1
 	southwin = win
 
-#FINALFOUR
+# FINALFOUR
 finals1 = probability(eastwin, westwin, i)
 i+=1
 finals2 = probability(midwestwin, southwin, i)
 i+=1
-#CHAMPTIONSHIP
+# CHAMPIONSHIP
 thewinner = probability(finals1, finals2, i)
 print(thewinner)
 
-#Output into tab delineated form. Best opened in excel.
+# Output into tab delineated form. Best opened in excel.
 with open(output, 'w') as f:
 	f.write('FIRSTROUND:\t')
 	for game in eastfirst:
